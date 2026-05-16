a5c500f197071f61c86e7fca31f3aa4df0174c35
bradleyng19 <flashghost7@gmail.com>
2026-05-16 18:49:59 +0800

    feature: Basic database setup and User authentication
    Set up client model and user authentication flow

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Client Model
+class Client(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100, blank=True)
+    service_type = models.CharField(max_length=100, blank=True)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ClientForm
+from .forms import ClientForm, UserSignUpForm
 from .models import Client
 
 @login_required
@@ -24,3 +24,16 @@ def client_list(request):
     clients = Client.objects.filter(owner=request.user)
 
     return render(request, "core/client_list.html", {"clients": clients})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = UserSignUpForm()
+
+    return render(request, "core/signup.html", {"form": form})
